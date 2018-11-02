196721d9399e0105a927b1989209dc64f469f413
LarisaStancu <43820483+LarisaStancu@users.noreply.github.com>
2018-11-02 22:08:25 +0200

    Add files via upload

---
+++ Problema 2.m
@@ -0,0 +1,20 @@
+%rezolutie temporara de 2 ms
+t=0:0.002:15;   %vom afisa 3 perioade
+x=1.5*sawtooth(2*pi*0.2*t); %pentru a avea A+=1 si A-=-2
+                                 %luam nivel=0.5 (media/2)
+                                 %amplitudine/procent =pk2pk/2=1,5
+subplot(3,1,1) %am folosit functia subplot pentru a avea toate graficele intr-o pagina
+plot(t,x),grid,xlabel('t [s]'),ylabel('A [V]')
+
+%rezolutie temporara de 20 ms
+t=0:0.02:15;   %vom afisa 3 perioade
+x=1.5*sawtooth(2*pi*0.2*t);
+subplot(3,1,2)
+plot(t,x),grid,xlabel('t [s]'),ylabel('A [V]')
+
+
+%rezolutie temporara de 200 ms
+t=0:0.2:15;   %vom afisa 3 perioade
+x=1.5*sawtooth(2*pi*0.2*t);
+subplot(3,1,3)
+plot(t,x),grid,xlabel('t [s]'),ylabel('A [V]')
